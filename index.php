0396db66d33100d59c4f2eaf4fbe9e0274e74393
Maks Kuzin <maks.kuzin5544@gmail.com>
2025-09-14 12:29:01 +0500

    now php

--- index.php
+++ index.php
@@ -18,9 +18,10 @@ $date = new DateTime('NOW');
 $hour = $date->format('G');
 if ($hour < 8 || $hour >= 23) {
     echo"<body class='night_theme'>";
+}else{
+    echo"<body>";
 }
 ?>
-  <body>
     <div class="list1">
       <header class="TEXT_menu">
         <div class="header_left">
